7476ba2b276c232c2209f1eb51f9f55f128f1030
sofiehyllen <sofiehyllen@hotmail.com>
2023-09-22 01:46:14 +0200

    commit inden wp button

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,6 @@
 import sofieSidder from "/src/assets/sofie.sidder.BW.jpg"
+import underConstruction from "/src/assets/under.construction.png"
+
 export default function Contact(){
     return(
         <section id="contact">
@@ -6,24 +8,23 @@ export default function Contact(){
             <p>SEND ME A MESSAGE</p>
             <a href="mailto:sofiehyllen@hotmail.com" target="blank">SOFIEHYLLEN@HOTMAIL.COM</a>
             <div id="contact-image"><img className="img-max" src={sofieSidder} alt="Sofie" /></div>
+
             <div className="contact-container">
-                <div className="contact-links" >
-                    <div className="contact-links-right" >
-                        <a href="https://www.facebook.com/sofiefuglsang.hyllen/" target="_blank">FACEBOOK</a> <br />
-                        <a href="https://www.linkedin.com/in/sofie-fuglsang-hyllen-b98a6224a/" target="_blank">LINKEDIN</a>
-                    </div>
+                <div className="contact-links-right" >
+                    <a href="https://www.facebook.com/sofiefuglsang.hyllen/" target="_blank">FACEBOOK</a> <br />
+                    <a href="https://www.linkedin.com/in/sofie-fuglsang-hyllen-b98a6224a/" target="_blank">LINKEDIN</a>
                 </div>
-                <div className="contact-links" >
-                    <div className="contact-links-left"  >
-                        <div id="instagram"><a >INSTAGRAM</a> <br /></div>
-                        <a href="mailto: sofiehyllen@hotmail.com" target="_blank">E-MAIL</a>
-                    </div>
-                </div>  
-         
+
+                <div className="contact-links-left"  >
+                    <div id="instagram"><a >INSTAGRAM</a> <br /></div>
+                    <a href="mailto: sofiehyllen@hotmail.com" target="_blank">E-MAIL</a>
+                </div>
+                <div className="instagram-illustration"><img className="img-max" src={underConstruction} alt="Under Construction" /></div>                    
             </div>
+
             <div className="bottom-text">
                 <p>&#169; SOFIE FUGLSANG HYLLEN 2023</p>
-                <p id="back-to-top"><a  href="#">BACK TO TOP</a></p>
+                <p id="back-to-top"><a  href="#">JUMP TO TOP</a></p>
             </div> 
         </section>
     );
